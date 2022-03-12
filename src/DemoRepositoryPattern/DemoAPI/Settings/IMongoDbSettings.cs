7f71eaa1842a984189aa32443e9504fd9516f159
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-03-12 10:47:35 +0200

    Add settings class to access injected values from appsettings.json

---
+++ src/DemoRepositoryPattern/DemoAPI/Settings/IMongoDbSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoAPI.Settings
+{
+    /// <summary>
+    /// Will hold the values from the 'MongoDbSettings' object in the 'appsettings.json' file
+    /// </summary>
+    public interface IMongoDbSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ src/DemoRepositoryPattern/DemoAPI/Settings/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace DemoAPI.Settings;
+
+public class MongoDbSettings : IMongoDbSettings
+{
+    public string DatabaseName { get; set; }
+    public string ConnectionString { get; set; }
+}
