4277e603bee6681d8e4fd1d3269e4114514b42fa
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-03-12 11:30:02 +0200

    add bson document abstraction

---
+++ src/DemoRepositoryPattern/DemoAPI/Domain/Document.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace DemoAPI.Domain
+{
+    internal abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+}
